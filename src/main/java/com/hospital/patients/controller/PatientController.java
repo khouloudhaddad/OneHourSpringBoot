bfe72b331fdae3c4640638a30bd467198f71650e
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-15 21:09:57 +0000

    Fix missing import

--- src/main/java/com/hospital/patients/controller/PatientController.java
+++ src/main/java/com/hospital/patients/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.hospital.patients.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class PatientController {
