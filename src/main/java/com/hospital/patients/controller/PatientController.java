248b8d8535f455251b3cf69256f84061f403be05
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-07-15 22:13:45 +0100

    Create first service- fix errors

--- src/main/java/com/hospital/patients/controller/PatientController.java
+++ src/main/java/com/hospital/patients/controller/PatientController.java
@@ -1,5 +1,6 @@
 package com.hospital.patients.controller;
 
+import com.hospital.patients.service.PatientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class PatientController {
 
     @Autowired
-    private PatientController partientController; //bean
+    private PatientService patientService;
 
     @GetMapping("/print-name")
     public String printPatientName() {
-        partientController.printName("khouloud");
+       return patientService.printName("khouloud");
     }
-    
+
 }
