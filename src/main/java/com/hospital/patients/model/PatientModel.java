0cd2ca720c3c1b62a334e341854b24ec26fa2dab
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-15 21:41:13 +0000

    Implement the Model

--- src/main/java/com/hospital/patients/model/PatientModel.java
+++ src/main/java/com/hospital/patients/model/PatientModel.java
@@ -1,5 +1,17 @@
 package com.hospital.patients.model;
 
-public class PatientModel {
+import java.util.List;
 
+public class PatientModel {
+    List<String> names = new ArrayList<>();
+
+    PatientModel() {
+        names.add("Frank");
+        names.add("Med");
+        names.add("Greg");
+    }
+    
+    public String getPatientFromDatabase() {
+        return names.get(0);
+    }
 }
--- src/main/java/com/hospital/patients/service/PatientService.java
+++ src/main/java/com/hospital/patients/service/PatientService.java
@@ -1,12 +1,17 @@
 package com.hospital.patients.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.hospital.patients.model.PatientModel;
 
 @Service
 public class PatientService {
 
+    @Autowired
+    private PatientModel patientModel;
+
     public String printName(String name) {
-        return name.toUpperCase();
+        return patientModel.getPatientFromDatabase().toUpperCase();
     }
     
 }
