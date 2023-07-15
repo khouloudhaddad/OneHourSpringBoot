9fce83430dcfbbb6248d96d8810ef9e3edf16f3b
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-07-15 21:20:57 +0100

    SpringBoot App

---
+++ src/main/java/com/hospital/patients/PatientsApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.patients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/patients/PatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.patients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
